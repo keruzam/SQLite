c32451d51267bc078b91a7c1fb39abd3199ee796
keruzam <mazurek.zg@gmail.com>
2016-09-29 23:39:29 +0200

    * AndroidManifest (min version 11) * MyDialogFragment * Menu->settings

--- app/src/main/java/sqlite/keruzam/pl/sqlite/MainActivity.java
+++ app/src/main/java/sqlite/keruzam/pl/sqlite/MainActivity.java
@@ -1,10 +1,13 @@
 package sqlite.keruzam.pl.sqlite;
 
+import android.app.DialogFragment;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -124,6 +127,45 @@ public class MainActivity extends AppCompatActivity {
         } catch (Exception e) {
             showToast(e.getMessage());
         }
-
     }
+
+    // This method creates the menu on the app
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    // Called when a options menu item is selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // We check what menu item was clicked and show a Toast
+        if (id == R.id.action_settings) {
+
+            // A DialogFragment is a Fragment you can place over top
+            // the current Activity. A Fragment is like an interface
+            // block that you can place into an Activity.
+            // The FrgamentManager allows you to interact with the
+            // Fragment
+            DialogFragment myFragment = new MyDialogFragment();
+
+            myFragment.show(getFragmentManager(), "dialog");
+
+            return true;
+
+            // If exit was clicked close the app
+        } else if (id == R.id.exit_the_app) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/sqlite/keruzam/pl/sqlite/MyDialogFragment.java
@@ -0,0 +1,35 @@
+package sqlite.keruzam.pl.sqlite;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by fuks on 2016-09-29.
+ */
+
+public class MyDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+        dialog.setTitle("Testowy dialog");
+        dialog.setMessage("Witaj!!");
+        dialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Clicked OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Clicked Cancel", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return super.onCreateDialog(savedInstanceState);
+    }
+}
